c5c4fec4a6700849d94c4e23baa70f0174c89d4a
vidhi <narodiyavidhi@gmail.com>
2023-08-01 12:29:54 +0530

    add comment

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import tensorflow as tf
 # model.fit(x_train, y_train, epochs=3)
 
 # model.save('handwritten.model')
-
+# v
 model = tf.keras.models.load_model('handwritten.model')
 
 image_number = 1
